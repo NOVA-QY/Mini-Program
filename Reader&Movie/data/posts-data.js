7bbebe52a1ac839047c77f6b202e5bd83f84c850
Homo_sapiens <820043860@qq.com>
2018-05-15 22:26:18 +0800

    submit

---
+++ Reader&Movie/data/posts-data.js
@@ -0,0 +1,90 @@
+var local_database = [
+    {
+        date: "Aug 18 2016",
+        title: "外面雷好大",
+        imgSrc: "/images/universe.jpg",
+        content: "我想回宿舍睡大觉",
+        reading: "13",
+        collection: "14",
+        avatar: "/images/avatar/1.png",
+        headImgSrc:"/images/universe.jpg",
+        author:"乔宇",
+        dateTime:"Nov 12 2017",
+        detail:"几天外面雷好大几天外面雷好大几天外面雷好大几天外面雷好大几天外面雷好大几天外面雷好大几天外面雷好大几天外面雷好大几天外面雷好大几天外面雷好大几天外面雷好大几天外面雷好大几天外面雷好大几天外面雷好大几天外面雷好大几天外面雷好大几天外面雷好大几天外面雷好大几天外面雷好大",
+        postId:0
+    },
+    {
+        date: "Nov 25 1997",
+        title: "比利.林恩的中场战事",
+        imgSrc: "/images/post/bl.png",
+        content: ">伊拉克战争时期，来自美国德州的19岁技术兵比利·林恩（乔·阿尔文 Joe Alwyn 饰）因为一段偶然拍摄的视频而家喻户晓。那是一次规模不大却激烈非常的遭遇战，战斗中林恩所在的B班班长（范·迪塞尔 Vin Diesel 饰）遭到当地武装分子的伏击和劫持，而林恩为了营救班长不惜铤而走险冲锋陷阵。",
+        reading: "90",
+        collection: "10000",
+        avatar: "/images/avatar/2.png",
+        headImgSrc: "/images/post/bl.png",
+        author: "比利",
+        dateTime: "July 9 2016",
+        detail: "伊拉克战争时期，来自美国德州的19岁技术兵比利·林恩（乔·阿尔文 Joe Alwyn 饰）因为一段偶然拍摄的视频而家喻户晓。那是一次规模不大却激烈非常的遭遇战，战斗中林恩所在的B班班长（范·迪塞尔 Vin Diesel 饰）遭到当地武装分子的伏击和劫持，而林恩为了营救班长不惜铤而走险冲锋陷阵。",
+        postId: 1
+    },
+    {
+        date: "Nov 25 1997",
+        title: "阿甘正传",
+        imgSrc: "/images/post/AGan.jpg",
+        content: ">伊拉克战争时期，来自美国德州的19岁技术兵比利·林恩（乔·阿尔文 Joe Alwyn 饰）因为一段偶然拍摄的视频而家喻户晓。那是一次规模不大却激烈非常的遭遇战，战斗中林恩所在的B班班长（范·迪塞尔 Vin Diesel 饰）遭到当地武装分子的伏击和劫持，而林恩为了营救班长不惜铤而走险冲锋陷阵。",
+        reading: "90",
+        collection: "10000",
+        avatar: "/images/avatar/3.png",
+        headImgSrc: "/images/post/AGan.jpg",
+        author: "阿甘",
+        dateTime: "Nov 25 1997",
+        detail: "伊拉克战争时期，来自美国德州的19岁技术兵比利·林恩（乔·阿尔文 Joe Alwyn 饰）因为一段偶然拍摄的视频而家喻户晓。那是一次规模不大却激烈非常的遭遇战，战斗中林恩所在的B班班长（范·迪塞尔 Vin Diesel 饰）遭到当地武装分子的伏击和劫持，而林恩为了营救班长不惜铤而走险冲锋陷阵",
+        postId: 2
+    },
+    {
+        date: "Nov 25 1997",
+        title: "星际穿越",
+        imgSrc: "/images/post/Interstellar.jpg",
+        content: ">伊拉克战争时期，来自美国德州的19岁技术兵比利·林恩（乔·阿尔文 Joe Alwyn 饰）因为一段偶然拍摄的视频而家喻户晓。那是一次规模不大却激烈非常的遭遇战，战斗中林恩所在的B班班长（范·迪塞尔 Vin Diesel 饰）遭到当地武装分子的伏击和劫持，而林恩为了营救班长不惜铤而走险冲锋陷阵。",
+        reading: "90",
+        collection: "10000",
+        avatar: "/images/avatar/4.png",
+        headImgSrc: "/images/post/Interstellar.jpg",
+        author: "诺兰",
+        dateTime: "Nov 25 1997",
+        detail: "伊拉克战争时期，来自美国德州的19岁技术兵比利·林恩（乔·阿尔文 Joe Alwyn 饰）因为一段偶然拍摄的视频而家喻户晓。那是一次规模不大却激烈非常的遭遇战，战斗中林恩所在的B班班长（范·迪塞尔 Vin Diesel 饰）遭到当地武装分子的伏击和劫持，而林恩为了营救班长不惜铤而走险冲锋陷阵。",
+        postId: 3
+    },
+    {
+        date: "Nov 25 1997",
+        title: "贫民窟的百万富翁",
+        imgSrc: "/images/post/Millionares.jpg",
+        content: ">伊拉克战争时期，来自美国德州的19岁技术兵比利·林恩（乔·阿尔文 Joe Alwyn 饰）因为一段偶然拍摄的视频而家喻户晓。那是一次规模不大却激烈非常的遭遇战，战斗中林恩所在的B班班长（范·迪塞尔 Vin Diesel 饰）遭到当地武装分子的伏击和劫持，而林恩为了营救班长不惜铤而走险冲锋陷阵。",
+        reading: "90",
+        collection: "10000",
+        avatar: "/images/avatar/5.png",
+        headImgSrc: "/images/post/Millionares.jpg",
+        author: "贫民窟",
+        dateTime: "Nov 25 1997",
+        detail: "伊拉克战争时期，来自美国德州的19岁技术兵比利·林恩（乔·阿尔文 Joe Alwyn 饰）因为一段偶然拍摄的视频而家喻户晓。那是一次规模不大却激烈非常的遭遇战，战斗中林恩所在的B班班长（范·迪塞尔 Vin Diesel 饰）遭到当地武装分子的伏击和劫持，而林恩为了营救班长不惜铤而走险冲锋陷阵。",
+        postId: 4
+    },
+    {
+        date: "Nov 25 1997",
+        title: "肖生克的救赎",
+        imgSrc: "/images/post/Shawshank.jpg",
+        content: ">伊拉克战争时期，来自美国德州的19岁技术兵比利·林恩（乔·阿尔文 Joe Alwyn 饰）因为一段偶然拍摄的视频而家喻户晓。那是一次规模不大却激烈非常的遭遇战，战斗中林恩所在的B班班长（范·迪塞尔 Vin Diesel 饰）遭到当地武装分子的伏击和劫持，而林恩为了营救班长不惜铤而走险冲锋陷阵。",
+        reading: "90",
+        collection: "10000",
+        avatar: "/images/avatar/5.png",
+        headImgSrc: "/images/post/Shawshank.jpg",
+        author: "肖生克",
+        dateTime: "Nov 25 1997",
+        detail: "伊拉克战争时期，来自美国德州的19岁技术兵比利·林恩（乔·阿尔文 Joe Alwyn 饰）因为一段偶然拍摄的视频而家喻户晓。那是一次规模不大却激烈非常的遭遇战，战斗中林恩所在的B班班长（范·迪塞尔 Vin Diesel 饰）遭到当地武装分子的伏击和劫持，而林恩为了营救班长不惜铤而走险冲锋陷阵。",
+        postId: 5
+    },
+]
+
+module.exports = {
+    postList:local_database
+}
---
+++ Reader&Movie/pages/posts/post-detail/post-detail.js
@@ -0,0 +1,112 @@
+var postsData = require('../../../data/posts-data.js')
+Page({
+    data:{
+        
+    },
+    onLoad:function(option){
+        var postId = option.id;
+        this.data.currentPostId = postId;    //1.中转获取postId
+        var postData = postsData.postList[postId];
+        this.setData({
+            postData:postData
+        })
+
+        // var postsCollected = {
+        //     1:"true",
+        //     2:"false",
+        //     3:"true"
+        //     ...
+        // }
+
+        //读取文章收藏与否的状态（所有的），键任意指定
+        var postsCollected = wx.getStorageSync('posts_collected')
+
+        //判断是否存在
+        if(postsCollected){
+            //获取其中一个是否已经缓存
+            var postCollected = postsCollected[postId]
+            //数据绑定
+            //将detail文章是否被收藏的状态绑定到collected变量里
+            if(postCollected){
+                this.setData({
+                    collected: postCollected
+                })
+            }
+        }
+        //若缓存结构体为空
+        else{
+            var postsCollected = {};
+            postsCollected[postId] = false;
+            wx.setStorageSync('posts_collected',postsCollected);
+        }  
+    },
+
+    onCollectionTap:function(event){
+        //通过缓存了解是否已经被收藏
+        var postsCollected = wx.getStorageSync('posts_collected');
+        var postCollected = postsCollected[this.data.currentPostId];   //2.中转获取postId
+        //取反：收藏变为未收藏，未收藏变为收藏
+        postCollected = !postCollected;     
+        //更新缓存
+        postsCollected[this.data.currentPostId] = postCollected;
+        this.showToast(postsCollected,postCollected);  //必须加this
+    },
+
+    showModal:function(postsCollected,postCollected){
+        var that = this;//保存this
+        wx.showModal({
+            title: '收藏',
+            content: postCollected?"收藏该文章？":"取消收藏该文章？",
+            showCancel:"true",
+            cancelText:"取消",
+            cancelColor:"#333",
+            confirmText:"确认",
+            confirmColor:"#405f80",
+            success:function(res){
+                if(res.confirm){
+                    //更新文件是否收藏的缓存值（相当于数据库更新）
+                    wx.setStorageSync('posts_collected', postsCollected);
+                    //更新数据绑定变量，从而实现切换图片
+                    that.setData({
+                        collected: postCollected
+                    })
+                }
+            }
+        })  
+    },
+
+    showToast:function(postsCollected,postCollected){
+        //更新文件是否收藏的缓存值（相当于数据库更新）
+        wx.setStorageSync('posts_collected', postsCollected);
+        //更新数据绑定变量，从而实现切换图片
+        this.setData({
+            collected: postCollected
+        })
+        wx.showToast({
+            title: postCollected ? "收藏成功" : "取消成功",
+            duration: 1000,
+            icon: "success"
+        })
+    },
+
+    onShareTap:function(event){
+        var itemList=[
+            "分享给微信好友",
+            "分享到朋友圈",
+            "分享到QQ",
+            "分享到微博"
+        ]
+        wx.showActionSheet({
+            itemList:itemList,
+            itemColor:"#405f80",
+            success:function(res){
+                // res.cancel 用户是否点击了取消按钮
+                // res.tapIndex 数组元素的序号，从0开始
+                wx.showModal({
+                    title:"用户"+itemList[res.tapIndex],
+                    content:"用户是否取消？"+res.cancel+"现在无法实现分享功能，什么时候能支持呢"
+                })
+            }
+        })
+    }
+})
