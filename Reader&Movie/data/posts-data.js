c024d32d7879c89713b44876fc997dd7534d0283
Homo_sapiens <820043860@qq.com>
2018-05-18 21:22:18 +0800

    update

--- Reader&Movie/data/posts-data.js
+++ Reader&Movie/data/posts-data.js
@@ -11,7 +11,12 @@ var local_database = [
         author:"乔宇",
         dateTime:"Nov 12 2017",
         detail:"几天外面雷好大几天外面雷好大几天外面雷好大几天外面雷好大几天外面雷好大几天外面雷好大几天外面雷好大几天外面雷好大几天外面雷好大几天外面雷好大几天外面雷好大几天外面雷好大几天外面雷好大几天外面雷好大几天外面雷好大几天外面雷好大几天外面雷好大几天外面雷好大几天外面雷好大",
-        postId:0
+        postId:0,
+        music:{
+            url: "http://music.163.com/song/media/outer/url?id=142604.mp3",
+            title: "夜夜夜夜-齐秦",
+            coverImg: "http://y.gtimg.cn/music/photo_new/T002R150x150M000001TEc6V0kjpVC.jpg?max_age=2592000"
+        }
     },
     {
         date: "Nov 25 1997",
@@ -25,7 +30,12 @@ var local_database = [
         author: "比利",
         dateTime: "July 9 2016",
         detail: "伊拉克战争时期，来自美国德州的19岁技术兵比利·林恩（乔·阿尔文 Joe Alwyn 饰）因为一段偶然拍摄的视频而家喻户晓。那是一次规模不大却激烈非常的遭遇战，战斗中林恩所在的B班班长（范·迪塞尔 Vin Diesel 饰）遭到当地武装分子的伏击和劫持，而林恩为了营救班长不惜铤而走险冲锋陷阵。",
-        postId: 1
+        postId: 1,
+        music: {
+            url: "http://music.163.com/song/media/outer/url?id=108220.mp3",
+            title: "鬼迷心窍-李宗盛",
+            coverImg: "http://y.gtimg.cn/music/photo_new/T002R150x150M000002xOmp62kqSic.jpg?max_age=2592000"
+        }
     },
     {
         date: "Nov 25 1997",
@@ -39,7 +49,12 @@ var local_database = [
         author: "阿甘",
         dateTime: "Nov 25 1997",
         detail: "伊拉克战争时期，来自美国德州的19岁技术兵比利·林恩（乔·阿尔文 Joe Alwyn 饰）因为一段偶然拍摄的视频而家喻户晓。那是一次规模不大却激烈非常的遭遇战，战斗中林恩所在的B班班长（范·迪塞尔 Vin Diesel 饰）遭到当地武装分子的伏击和劫持，而林恩为了营救班长不惜铤而走险冲锋陷阵",
-        postId: 2
+        postId: 2,
+        music: {
+            url: "http://music.163.com/song/media/outer/url?id=27538254.mp3",
+            title: "女儿情-万晓利",
+            coverImg: "http://y.gtimg.cn/music/photo_new/T002R150x150M000004Wv5BO30pPc0.jpg?max_age=2592000"
+        }
     },
     {
         date: "Nov 25 1997",
@@ -53,7 +68,12 @@ var local_database = [
         author: "诺兰",
         dateTime: "Nov 25 1997",
         detail: "伊拉克战争时期，来自美国德州的19岁技术兵比利·林恩（乔·阿尔文 Joe Alwyn 饰）因为一段偶然拍摄的视频而家喻户晓。那是一次规模不大却激烈非常的遭遇战，战斗中林恩所在的B班班长（范·迪塞尔 Vin Diesel 饰）遭到当地武装分子的伏击和劫持，而林恩为了营救班长不惜铤而走险冲锋陷阵。",
-        postId: 3
+        postId: 3,
+        music: {
+            url: "http://music.163.com/song/media/outer/url?id=108119.mp3",
+            title: "恋恋风尘-老狼",
+            coverImg: "http://y.gtimg.cn/music/photo_new/T002R150x150M000001VaXQX1Z1Imq.jpg?max_age=2592000"
+        }
     },
     {
         date: "Nov 25 1997",
@@ -67,7 +87,12 @@ var local_database = [
         author: "贫民窟",
         dateTime: "Nov 25 1997",
         detail: "伊拉克战争时期，来自美国德州的19岁技术兵比利·林恩（乔·阿尔文 Joe Alwyn 饰）因为一段偶然拍摄的视频而家喻户晓。那是一次规模不大却激烈非常的遭遇战，战斗中林恩所在的B班班长（范·迪塞尔 Vin Diesel 饰）遭到当地武装分子的伏击和劫持，而林恩为了营救班长不惜铤而走险冲锋陷阵。",
-        postId: 4
+        postId: 4,
+        music: {
+            url: "http://music.163.com/song/media/outer/url?id=188204.mp3",
+            title: "沉默是金-张国荣",
+            coverImg: "http://y.gtimg.cn/music/photo_new/T002R150x150M000003at0mJ2YrR2H.jpg?max_age=2592000"
+        }
     },
     {
         date: "Nov 25 1997",
@@ -81,7 +106,12 @@ var local_database = [
         author: "肖生克",
         dateTime: "Nov 25 1997",
         detail: "伊拉克战争时期，来自美国德州的19岁技术兵比利·林恩（乔·阿尔文 Joe Alwyn 饰）因为一段偶然拍摄的视频而家喻户晓。那是一次规模不大却激烈非常的遭遇战，战斗中林恩所在的B班班长（范·迪塞尔 Vin Diesel 饰）遭到当地武装分子的伏击和劫持，而林恩为了营救班长不惜铤而走险冲锋陷阵。",
-        postId: 5
+        postId: 5,
+        music: {
+            url: "http://music.163.com/song/media/outer/url?id=152428.mp3",
+            title: "朋友-谭咏麟",
+            coverImg: "http://y.gtimg.cn/music/photo_new/T002R150x150M000004eGsCN3SUheO.jpg?max_age=2592000"
+        }
     },
 ]
 
