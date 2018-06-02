1fbf55bcd1041d12e0a713fc19c0ee298de58ff2
Homo_sapiens <820043860@qq.com>
2018-06-02 17:52:34 +0800

    增加了util存放公共函数

--- Reader&Movie/pages/movies/movies.js
+++ Reader&Movie/pages/movies/movies.js
@@ -1,17 +1,25 @@
+var util = require('../../utils/util.js')
 var app = getApp();
 Page({
+    data:{
+        inTheaters:{},
+        comingSoon:{},
+        top250:{}
+    },
+
     onLoad: function (event) {
         var inTheatersUrl = app.globalData.doubanBase + "/v2/movie/in_theaters"+"?start=0&count=3";
         var comingSoonUrl = app.globalData.doubanBase + "/v2/movie/coming_soon" + "?start=0&count=3";
         var top250Url = app.globalData.doubanBase + "/v2/movie/top250" + "?start=0&count=3";
 
-        this.getMovieListData(inTheatersUrl);
-        // this.getMovieListData(comingSoonUrl);
-        // this.getMovieListData(top250Url);
+        this.getMovieListData(inTheatersUrl,"inTheaters","正在热映");
+        this.getMovieListData(comingSoonUrl,"comingSoon","即将上映");
+        this.getMovieListData(top250Url,"top250","豆瓣Top250");
 
     },
 
-    getMovieListData: function (url) {
+// 获取豆瓣的数据
+    getMovieListData: function (url,settedKey,categoryTitle) {
         var that = this;
         wx.request({
             url: url,
@@ -21,14 +29,15 @@ Page({
             },
             success: function (res) {
                 console.log(res);
-                that.processDoubanData(res.data)
+                that.processDoubanData(res.data,settedKey,categoryTitle)
             },
             fail: function () {
             },
         })
     },
 
-    processDoubanData:function(moviesDouban){
+    // 处理获得的豆瓣数据
+    processDoubanData:function(moviesDouban,settedKey,categoryTitle){
         var movies = [];
         for(var idx in moviesDouban.subjects){
             var subject = moviesDouban.subjects[idx];
@@ -36,7 +45,9 @@ Page({
             if(title.length >= 6){
                 title = title.substring(0,6) + "...";
             }
+            // [1,1,1,1,1] [1,1,1,0,0]
             var temp = {
+                stars:util.convertToStarsArray(subject.rating.stars),
                 title: title,
                 average:subject.rating.average,
                 coverageUrl:subject.images.large,
@@ -44,8 +55,13 @@ Page({
             }
             movies.push(temp)
         }
-        this.setData({
+
+        //动态赋值JS属性
+        var readyData={};
+        readyData[settedKey] = {
+            categoryTitle:categoryTitle,
             movies:movies
-        })
+        }
+        this.setData(readyData);
     }
 })
