22e918b86dcf44b949b8726a6d3640157fa7dda8
Homo_sapiens <820043860@qq.com>
2018-06-03 10:53:33 +0800

    添加了http function

--- Reader&Movie/utils/util.js
+++ Reader&Movie/utils/util.js
@@ -1,16 +1,42 @@
-function convertToStarsArray(stars){
-    var num = stars.toString().substring(0,1);
+/*
+    存放公共函数
+*/
+
+
+function convertToStarsArray(stars) {
+    var num = stars.toString().substring(0, 1);
     var array = [];
-    for(var i=1;i<=5;i++){
-        if(i<=num){
+    for (var i = 1; i <= 5; i++) {
+        if (i <= num) {
             array.push(1);
-        }else{
+        } else {
             array.push(0);
         }
     }
     return array;
 }
 
+// 获取豆瓣的数据
+// 异步方法，需要回调函数
+// 可拓展：添加method、post等参数进行传参（服务器代码）
+function http(url,callBack){
+    wx.request({
+        url: url,
+        method: 'GET',
+        header: {
+            "content-type": "json"
+        },
+        success: function (res) {
+            callBack(res.data);
+        },
+        fail: function (error) {
+            console.log(error)
+        }
+    })
+}
+
 module.exports = {
-    convertToStarsArray:convertToStarsArray,
+    convertToStarsArray: convertToStarsArray,
+    // 输出
+    http:http
 }
